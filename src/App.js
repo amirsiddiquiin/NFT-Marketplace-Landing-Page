82a36d5180f2320235bdfad138eefc2d7de0f748
amir.in <amirsiddiqui.in@gmail.com>
2021-06-09 15:47:47 +0530

    Fix vuild error

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import './App.css';
-import Home from './Pages/Home';
-import Navbar from './Components/Common/Navbar';
-import Corgib from './Components/Common/Corgib';
-import WhyCorgib from './Components/Common/WhyCorgib';
-import Marketplace from './Components/Common/Marketplace';
-import Tokenomics from './Components/Common/Tokenomics';
-import Roadmap from './Pages/Roadmap';
-import Buy from './Pages/Buy';
-import Footer from './Components/Common/Footer';
+import LandingPage from './Components/Home/LandingPage';
+import Navbar from './Components/Home/Navbar';
+
 function App() {
   return (
     <div className="App">
      <Navbar/>
-     <Home/>
-     <Corgib/>
-     <WhyCorgib/>
-     <Marketplace/>
-     <Tokenomics/>
-     <Roadmap/>
-     <Buy/>
-     <Footer/>
+     <LandingPage/>
     </div>
   );
 }
-
 export default App;
---
+++ src/Components/Home/LandingPage.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Corgib from './Corgib';
+import WhyCorgib from './WhyCorgib';
+import Marketplace from './Marketplace';
+import Tokenomics from './Tokenomics';
+import Roadmap from './Roadmap';
+import Footer from './Footer';
+import Home from './Home';
+import Buy from './Buy';
+
+
+function LandingPage() {
+  return (
+    <div>
+      <Home/>
+     <Corgib/>
+     <WhyCorgib/>
+     <Marketplace/>
+     <Tokenomics/>
+     <Roadmap/>
+     <Buy/>
+     <Footer/>
+    </div>
+  )
+}
+
+export default LandingPage
